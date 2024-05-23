a6aa915f893af0593f65ff44a530bd52561e81ca
lucas-eichhorn-ext <lucas.eichhorn-ext@globalservs.com>
2024-05-23 23:37:03 +0200

    add export

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/sai-manual-conectar",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
